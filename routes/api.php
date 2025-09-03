92fb51ff85702dc59446aa347e3eed9618e06fa7
Kevin <kevinbuangjog@gmail.com>
2025-09-03 17:58:44 +0800

    Added Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\NoteController; // Added controller for notes
-use GuzzleHttp\Psr7\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/hello', function () {
-    return 'Hello, World!';
-});
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 
-// Adding route to notes controller
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
